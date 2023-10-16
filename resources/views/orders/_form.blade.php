962d9cc8f2e0e2b2a8fd3c73b03a088854f7d22e
Joe <joe@unisolv.co.za>
2023-10-16 05:53:42 +0200

    tablet layout update

--- resources/views/orders/_form.blade.php
+++ resources/views/orders/_form.blade.php
@@ -47,7 +47,7 @@
                 <tr>
                     @if($tax_per_item and $discount_per_item)
                         <th >{{ __('global.products') }}</th>
-                        <th >{{ __('global.taxes') }}</th>
+                        <th style="visibility:hidden;">{{ __('global.taxes') }}</th>
                         <th >{{ __('global.quantity') }}</th>
                         <th >{{ __('global.stockOnhand') }}</th>
                         <th >{{ __('global.price') }}</th>
