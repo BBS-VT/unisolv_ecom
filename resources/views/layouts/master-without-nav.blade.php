b56800d44efad823db5f88d86e9e9a6129b45db6
JoeAppelgryn <joe@unisolv.co.za>
2024-11-26 15:28:04 +0200

    ui upgrade - login screen

--- resources/views/layouts/master-without-nav.blade.php
+++ resources/views/layouts/master-without-nav.blade.php
@@ -4,7 +4,7 @@
 <head>
 
     <meta charset="utf-8" />
-    <title> @yield('title') || {{ config('app.name', 'Unisolv CRM') }}</title>
+    <title> @yield('title') | {{ config('app.name', 'Unisolv CRM') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
