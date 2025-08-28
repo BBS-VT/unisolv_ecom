addeb466f4b3cf28db884af4fb6798d9e95f0f30
JoeAppelgryn <joe@unisolv.co.za>
2025-08-28 14:17:30 +0200

    update shop home page

--- resources/views/shop/layouts/partials/modals/minicart.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="offcanvas offcanvas-end" tabindex="-1" id="miniCartOffcanvas" style="width: 400px;">
-    <div id="miniCartContent">
-        <div class="d-flex justify-content-center align-items-center h-100">
-            <div class="spinner-border text-warning" role="status">
-                <span class="visually-hidden">Loading...</span>
-            </div>
-        </div>
-    </div>
-</div>
